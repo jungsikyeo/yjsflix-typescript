b3f2a56cbc3e3a56d5b1a002fcaacc2f38ac3fc9
jungsikyeo <mdotcom12@naver.com>
2021-06-30 00:08:13 +0900

    carousel add

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -13,18 +13,13 @@ const Carousel = ({ trendingData }: any) => {
   };
   console.log(trendingData);
   return (
-    <div className="h-screen">
-      <Slider {...settings}>
-        {trendingData.map((data: any) => (
-          <div>
-            <img
-              src={`https://image.tmdb.org/t/p/original${data.poster_path}`}
-              alt={data.original_title}
-            />
-          </div>
-        ))}
-      </Slider>
-    </div>
+    <Slider {...settings} className="h-screen">
+      {trendingData.map((data: any) => (
+        <div key={data.id} className="relative w-screen h-screen">
+          <div className="w-full h-screen absolute top-0 left-0 bg-center bg-cover opacity-60 z-0" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${data.backdrop_path})`}} />
+        </div>
+      ))}
+    </Slider>
   );
 };
 
