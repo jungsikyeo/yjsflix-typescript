3748b47d7045d2da5f5bd00bd216c9d7a83a0ac7
YJS <mdotcom12@naver.com>
2021-07-01 07:13:34 +0900

    header size fixed

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,13 +7,13 @@ export default withRouter(({ location: { pathname } }) => (
   <header className="fixed top-0 left-0 w-full h-14 px-4 flex items-center bg-black opacity-80 z-50 shadow-header text-white text-xl font-medium">
     <div className="w-full flex justify-between">
       <ul className="flex">
-        <li className="w-32 h-14">
+        <li className="w-20 md:w-32 h-14">
           <Link to="/" className="h-14 flex justify-start items-center">
             <img src={yjsflix} alt="yjsflix" className="w-28" />
           </Link>
         </li>
         <li
-          className={`w-20 h-14 ml-10 text-center border-b-2 ${
+          className={`w-10 md:w-20 h-14 ml-5 md:ml-10 text-center border-b-2 ${
             pathname === "/"
               ? `border-red-500`
               : `border-transparent font-extralight opacity-40`
@@ -24,7 +24,7 @@ export default withRouter(({ location: { pathname } }) => (
           </Link>
         </li>
         <li
-          className={`w-20 h-14 ml-10 text-center border-b-2 ml-4 ${
+          className={`w-10 md:w-20 h-14 ml-5 md:ml-10 text-center border-b-2 ml-4 ${
             pathname.indexOf("/movie") >= 0
               ? `border-red-500`
               : `border-transparent font-extralight opacity-40`
@@ -35,7 +35,7 @@ export default withRouter(({ location: { pathname } }) => (
           </Link>
         </li>
         <li
-          className={`w-20 h-14 text-center border-b-2 ml-4 ${
+          className={`w-10 md:w-20 h-14 ml-5 md:ml-10 text-center border-b-2 ml-4 ${
             pathname.indexOf("/tv") >= 0 || pathname.indexOf("/show") >= 0
               ? `border-red-500`
               : `border-transparent font-extralight opacity-40`
@@ -48,7 +48,7 @@ export default withRouter(({ location: { pathname } }) => (
       </ul>
       <ul className="flex">
         <li
-          className={`w-20 h-14 text-center border-b-2 ${
+          className={`w-10 md:w-20 h-14 ml-5 md:ml-10 text-center border-b-2 ${
             pathname === "/search" ? `border-red-700` : `border-transparent`
           }`}
         >
