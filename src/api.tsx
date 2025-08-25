e5ac25c776952cedd2397975771747de87036c32
여정식 <mdotcom12@naver.com>
2025-08-25 15:44:00 +0900

    Update api.tsx
    tmdb api key changed

--- src/api.tsx
+++ src/api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: "d2a00cc1f1a10798ef46eaf4ce97f962",
+    api_key: "34bdfca73e8fbc4767fb53ffaa04c96f",
     language: "en-US",
   },
 });
